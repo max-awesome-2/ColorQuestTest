6994b5027c1b8482e8292c118ddd554fe5bfd906
max-awesome-2 <gelinama@sheridancollege.ca>
2025-10-27 17:22:43 -0400

    Update video.js
    Trying to exactly match the stackoverflow post...

--- video.js
+++ video.js
@@ -11,69 +11,70 @@ function debugLog(text) {
 console.log("test");
 
 // https://stackoverflow.com/questions/69326902/html-canvas-video-streaming
-(function() {
-    var canvas= document.getElementById('canvas'),
-    context = canvas.getContext('2d'),
-    video = document.getElementById('video'),
-    vendorUrl = window.URL || window.webkitURL;
 
-    /*
-    navigator.getMedia = navigator.getUserMedia ||
-                         navigator.webkitGetUserMedia ||
-                         navigator.mozGetUserMedia ||
-                         navigator.msGetUserMedia;
-    */
+var canvas= document.getElementById('canvas'),
+context = canvas.getContext('2d'),
+video = document.getElementById('video'),
+vendorUrl = window.URL || window.webkitURL;
 
-    // https://stackoverflow.com/questions/53483975/navigator-mediadevices-getusermedia-not-working-on-ios-12-safari
-    video.setAttribute('autoplay', '');
-    video.setAttribute('muted', '');
-    video.setAttribute('playsinline', '');
-    //
+/*
+navigator.getMedia = navigator.getUserMedia ||
+                        navigator.webkitGetUserMedia ||
+                        navigator.mozGetUserMedia ||
+                        navigator.msGetUserMedia;
+*/
 
-    /*
-    navigator.getMedia ({
-        video: true,
-        audio: false
-    }, function(stream) {
-        debugLog("got stream");
-        //video.src = vendorUrl.createObjectURL(stream);
-        if ('srcObject' in video) {
-            debugLog("video has srcobj");
-            video.srcObject = stream;
-            debugLog("set video srcobj to stream");
-          } else {
-            debugLog("set video srcobj to createobjecturl stream");
-            video.src = vendorUrl.createObjectURL(stream);
-          }
-        video.play();
-        debugLog("playing video");
-    }, function(error) {
-        // An error occured
-        debugLog("error: " + error);
-    });
-    */
+// https://stackoverflow.com/questions/53483975/navigator-mediadevices-getusermedia-not-working-on-ios-12-safari
+video.setAttribute('autoplay', '');
+video.setAttribute('muted', '');
+video.setAttribute('playsinline', '');
+//
 
-    // https://stackoverflow.com/questions/72936834/capturing-camera-stream-in-three-js-on-ios
-    constraints = { audio: false, video: true };
-    navigator.mediaDevices.getUserMedia(constraints)
-    .then(function (stream) {
-        console.log("success");
+// https://stackoverflow.com/questions/72936834/capturing-camera-stream-in-three-js-on-ios
+constraints = { audio: false, video: true };
+navigator.mediaDevices.getUserMedia(constraints)
+.then(function (stream) {
+    console.log("success");
+    video.srcObject = stream;
+    video.play();
+})
+.catch(function(err) {
+    console.log('ERROR: ', err);
+});
+
+/*
+navigator.getMedia ({
+    video: true,
+    audio: false
+}, function(stream) {
+    debugLog("got stream");
+    //video.src = vendorUrl.createObjectURL(stream);
+    if ('srcObject' in video) {
+        debugLog("video has srcobj");
         video.srcObject = stream;
-        video.play();
-    })
-    .catch(function(err) {
-        console.log('ERROR: ', err);
-    });
+        debugLog("set video srcobj to stream");
+        } else {
+        debugLog("set video srcobj to createobjecturl stream");
+        video.src = vendorUrl.createObjectURL(stream);
+        }
+    video.play();
+    debugLog("playing video");
+}, function(error) {
+    // An error occured
+    debugLog("error: " + error);
+});
+*/
 
 
-    video.addEventListener('play', function() {
-        setInterval(() => {
-        draw(this, context, 400, 300);
-        }, 100);
 
-    }, false );
-    function draw(video, context, width, height) {
-        context.drawImage(video, 0, 0, width, height);
-    }
-    
-}) ();
+/*
+video.addEventListener('play', function() {
+    setInterval(() => {
+    draw(this, context, 400, 300);
+    }, 100);
+
+}, false );
+function draw(video, context, width, height) {
+    context.drawImage(video, 0, 0, width, height);
+}
+*/
