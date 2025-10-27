10eb83df0b5892c9f9296eb5a7d2d1b65e871a8a
max-awesome-2 <gelinama@sheridancollege.ca>
2025-10-27 14:37:44 -0400

    Testing StackOverflow code
    Added some test code from stackoverflow for capturing video

---
+++ video.js
@@ -0,0 +1,42 @@
+
+// https://stackoverflow.com/questions/69326902/html-canvas-video-streaming
+
+(function() {
+    var canvas= document.getElementById('canvas'),
+    context = canvas.getContext('2d'),
+    video = document.getElementById('video'),
+    vendorUrl = window.URL || window.webkitURL;
+
+    navigator.getMedia = navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia ||
+                         navigator.msGetUserMedia;
+
+    navigator.getMedia ({
+        video: true,
+        audio: false
+    }, function(stream) {
+        //video.src = vendorUrl.createObjectURL(stream);
+        if ('srcObject' in video) {
+            video.srcObject = stream;
+          } else {
+            video.src = vendorUrl.createObjectURL(stream);
+          }
+        video.play();
+    }, function(error) {
+        // An error occured
+        //error.code
+    });
+
+
+    video.addEventListener('play', function() {
+        setInterval(() => {
+        draw(this, context, 400, 300);
+        }, 100);
+
+    }, false );
+    function draw(video, context, width, height) {
+        context.drawImage(video, 0, 0, width, height);
+    }
+    
+}) ();
